9bf08f003b2f1f7203a97985d8103f37996dbee4
NephygTdkingz <tdkingzict@gmail.com>
2025-09-02 23:25:34 +0100

    prepared the project for vercel

--- miclink/urls.py
+++ miclink/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from frontend import views
 
@@ -10,3 +12,5 @@ urlpatterns = [
     path('ajax-login/', views.ajax_login_view, name='ajax_login'),
     path('statistics/', views.view_site_stats, name='view_site_stats'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
